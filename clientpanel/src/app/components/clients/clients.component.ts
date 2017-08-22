8adb892f7722e65f213738bbfb28f9fb476d2d5d
Rittick <drittick@gmail.com>
2017-08-22 14:30:56 -0500

    Total Owed Defined

--- clientpanel/src/app/components/clients/clients.component.ts
+++ clientpanel/src/app/components/clients/clients.component.ts
@@ -7,7 +7,9 @@ import { Client } from '../../models/Client';
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
-  clients:Client[];
+  clients:any[];
+  totalOwed:number;
+
   constructor(
     public clentService: ClientService
   ) { }
@@ -15,8 +17,17 @@ export class ClientsComponent implements OnInit {
   ngOnInit() {
     this.clentService.getClients().subscribe(clients => {
       this.clients = clients;
-      console.log(this.clients);
+      this.getTotalOwed();
     });
+  }
+
+  getTotalOwed(){
+    let total = 0;
+    for(let i = 0;i< this.clients.length;i++){
+      total += parseFloat(this.clients[i].balance);
+    }
+    this.totalOwed = total;
+    console.log(this.totalOwed);
   }
 
 }
